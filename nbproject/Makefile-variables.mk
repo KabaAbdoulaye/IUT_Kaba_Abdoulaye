3777aaf325e066af9a2b80bcf85d47bbaf3b52fc
TABLE3 <touloniut@gmail.com>
2022-09-13 17:34:47 +0200

    Second commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=moteur_robot_kaba_abdoulaye.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/moteur_robot_kaba_abdoulaye.X.production.hex
